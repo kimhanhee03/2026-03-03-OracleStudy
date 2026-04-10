e011fbed76913383e893b8833e3ad303321c588a
sist-9 <sist-9@sist9>
2026-04-10 16:15:35 +0900

    1111

---
+++ OracleFirstProject/src/com/sist/db/EmpMain.java
@@ -0,0 +1,34 @@
+package com.sist.db;
+import java.sql.*;
+import java.util.*;
+public class EmpMain {
+  
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		System.out.print("직위:");
+		String name=scan.next();
+		// 1.드라이버
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		String url="jdbc:oracle:thin:@localhost:1521:XE";
+		// 2.연결
+		Connection conn=DriverManager.getConnection(url,"hr","happy");
+		// Socket
+		// 3.명령문 전송
+		String sql="SELECT empno,ename,job FROM emp "
+				+"WHERE job LIKE '%"+name+"%'";
+		PreparedStatement ps=conn.prepareStatement(sql);
+		// OutputStream
+		// 4.실행결과값
+		ResultSet rs=ps.executeQuery();
+		// 5.출력
+		while(rs.next())
+		{
+			System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3));
+		}
+		rs.close();
+		ps.close();
+		conn.close();
+	}
+
+}
